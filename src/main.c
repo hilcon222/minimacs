361f9246881e39854ce6c9d83564c8aed9317468
Elia Toselli <elia.toselli@outlook.it>
2024-08-15 19:53:40 +0200

    it takes characters in input

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <string.h>
+#include <stdlib.h>
 #ifdef PROD
 #include <config.h>
 #else
@@ -12,10 +13,13 @@
 #define PACKAGE_VERSION "0.1"
 #define VERSION "0.1"
 #endif
-#define VERSION "0.1"
-
 
 int rows, cols;
+
+char *buffer;
+char *pos;
+int maxsiz;
+int inpsize;
 
 void firstLine(void) {
     getmaxyx(stdscr, rows, cols);
@@ -55,14 +59,42 @@ void init_program() {
     keypad(stdscr, true);
 }
 
+int inpchar(void) {
+    extern int maxsiz, inpsize;
+    extern char *buffer;
+    int c = getch();
+    if (inpsize == maxsiz) {
+        if ((buffer = (char *) realloc(buffer, maxsiz + BUFSIZ)) == NULL) {
+            endprog();
+            fprintf(stderr, "Too little memory space.\n");
+            exit(1);
+        }
+        maxsiz += BUFSIZ;
+    }
+    inpsize++;
+    addch(c);
+    *(pos++) = c;
+}
+
 int main() {
+    extern char * buffer;
     init_program();
     getmaxyx(stdscr, rows, cols);
     firstLine();
     attrset(A_NORMAL);
-
+    buffer = (char *) malloc(BUFSIZ);
+    extern int maxsiz;
+    maxsiz = BUFSIZ;
+    extern int inpsize;
+    inpsize = 0;
+    int c;
+    pos = buffer;
     lastLine();
+    move(1,0);
     attrset(A_NORMAL);
-    getch();
+    while (1) {
+        c = inpchar();
+    }
+
     endprog();
 }
